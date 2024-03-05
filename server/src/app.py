c01aafd733f26c8916c14d3a5543d6caa7adc5a6
1Dips <ido.geva.b@gmail.com>
2024-03-05 22:47:03 +0200

    add login page

--- server/src/app.py
+++ server/src/app.py
@@ -5,12 +5,15 @@ from .blueprints.stash_api import stash_api
 
 app = Flask(__name__)
 
+app.secret_key = "62tTuCMjXgCkPjw/B2AzvWLDe5Dz8UnonnD0TQnHG9jDxybe4Loo6uHz/jEckm/O"
+
 app.register_blueprint(stash_api, url_prefix="/stash")
 app.register_blueprint(auth, url_prefix="/auth")
 
 
 @app.route("/")
 def landing_page():
+    """Main page"""
     return render_template("main_page.html")
 
 
--- server/src/blueprints/auth.py
+++ server/src/blueprints/auth.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template
+"""
+Module
+"""
+from flask import Blueprint, render_template, session, request
+import bcrypt
 
 auth = Blueprint("auth", __name__)
 
+users = {
+    1: {
+        "username": "root",
+        "password":  bcrypt.hashpw(b'root', bcrypt.gensalt()),
+    }
+}
 
-@auth.route("/register")
+
+@auth.route("/register", methods=['POST', 'GET'])
 def register_user():
+    """Route responsible for handling user register"""
     return render_template("auth/register.html")
+
+
+@auth.route("/login", methods=['POST', 'GET'])
+def login_user():
+    """Route responsible for handling user login"""
+    if request.method == "GET":
+        return render_template("auth/login.html")
+
+    password = request.form.get("password")
+    username = request.form.get("username")
+
+    if bcrypt.checkpw(bytes(request.form.get("password"), encoding="utf-8"), users.get(1).get("password")):
+        return "OK"
+    return "NOT REQUESTED"
+
